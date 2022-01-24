b8d1d6ed2b01d7e39ef89435d43f88b1a5601b00
renaldo_louis <renaldo.louis@commsult.id>
2022-01-24 14:34:47 +0700

    mobile navbar

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,6 +1,29 @@
+import React, { useState, useEffect } from "react";
+
+
 export default function Menu({ menuOpen, setMenuOpen }) {
+    const [width, setWidth] = useState(window.innerWidth);
+    const [height, setHeight] = useState(window.innerHeight);
+
+    let isMobile = width <= 991;
+
+    function handleWindowSizeChange() {
+        setWidth(window.innerWidth);
+    }
+    function handleWindowHeightSizeChange() {
+        setHeight(window.innerHeight);
+    }
+
+    useEffect(() => {
+        window.addEventListener("resize", handleWindowSizeChange);
+        window.addEventListener("resize", handleWindowHeightSizeChange);
+        return () => {
+            window.removeEventListener("resize", handleWindowSizeChange);
+            window.removeEventListener("resize", handleWindowHeightSizeChange);
+        };
+    }, [isMobile, height]);
     return (
-        <div className={"menu " + (menuOpen && "active")}>
+        <div className={"menu " + (menuOpen && "active")} style={{ display: isMobile ? "none" : "" }}>
             <ul>
                 <li onClick={() => setMenuOpen(false)}>
                     <a href="#intro">Home</a>
